e4997072df6d2e153ca238d667f99914aa79da10
GiannyFrankGaleano <giannyfrank11@gmail.com>
2022-06-25 08:39:52 -0500

    primer ciclo

--- src/numeros.java
+++ src/numeros.java
@@ -1,7 +1,7 @@
 public class numeros {
 
     public static void main(String[] args) {
-        for (int i = 0; i<=100;i++){
+        for (int i = 0; i<=1000;i++){
             System.out.println("Numero:  "+i);
 
         }
